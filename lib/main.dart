688655e30c5df8d1da412d70a8e6ed2a889d46c3
hikio-17 <fajri010217@gmail.com>
2023-11-27 19:07:12 +0700

    create dropdown menu

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Demo'),
-        ),
-        body: const Center(
-          child: BiggerText(text: "Hello World!"),
-        ),
-      ),
+      home: const FirstScreen(),
     );
   }
 }
@@ -51,6 +44,7 @@ class BiggerText extends StatefulWidget {
 }
 
 class _BiggerTextState extends State<BiggerText> {
+
   double _textSize = 16.0;
 
   @override
@@ -68,6 +62,89 @@ class _BiggerTextState extends State<BiggerText> {
           },
         )
       ],
+    );
+  }
+}
+
+// class FirstScreen extends StatelessWidget {
+
+//   const FirstScreen({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('First Screen'),
+//         actions: <Widget>[
+//           IconButton(
+//             icon: const Icon(
+//               Icons.search,
+//               color: Colors.black,
+//             ),
+//             onPressed: () {},
+//           ),
+//         ],
+//         leading: IconButton(
+//           icon: const Icon(
+//             Icons.menu,
+//             color: Colors.black,
+//           ),
+//           onPressed: () {},
+//         ),
+//       ),
+//       body: const Center(
+//         child: Text('Hello World'),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         child: const Icon(
+//           Icons.add,
+//         ),
+//         onPressed: () {},
+//       ),
+//     );
+//   }
+// }
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+
+class _FirstScreenState extends State<FirstScreen> {
+  String? language;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: DropdownButton<String>(
+        items: const <DropdownMenuItem<String>>[
+          DropdownMenuItem<String>(
+            value: 'Dart',
+            child: Text('Dart'),
+          ),
+          DropdownMenuItem<String>(
+            value: 'Kotlin',
+            child: Text('Kotlin'),
+          ),
+          DropdownMenuItem<String>(
+            value: 'Swift',
+            child: Text('Swift'),
+          ),
+      ],
+      value: language,
+      hint: const Text('Select Language'),
+      onChanged: (String? value) {
+        setState(() {
+          language = value;
+        });
+      },
+      ),
     );
   }
 }
