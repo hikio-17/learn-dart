e7e13f95e55d08d98319fa2e14681c1e59f2caac
hikio-17 <fajri010217@gmail.com>
2023-11-27 16:21:23 +0700

    stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
           title: const Text('Flutter Demo'),
         ),
         body: const Center(
-          child: Heading(text: "Hello World!"),
+          child: BiggerText(text: "Hello World!"),
         ),
       ),
     );
@@ -37,6 +37,37 @@ class Heading extends StatelessWidget {
         fontSize: 24.0,
         fontWeight: FontWeight.bold,
       ),
+    );
+  }
+}
+
+class BiggerText extends StatefulWidget {
+  final String text;
+
+  const BiggerText({Key? key, required this.text}) : super(key: key);
+
+  @override
+  _BiggerTextState createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textSize)),
+        ElevatedButton(
+          child: const Text("Perbesar"),
+          onPressed: () {
+            setState(() {
+              _textSize = 32.0;
+            });
+          },
+        )
+      ],
     );
   }
 }
